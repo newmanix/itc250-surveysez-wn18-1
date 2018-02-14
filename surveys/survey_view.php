d4b1cba9008a3ade723dc886ace34a46b51e58db
Bill Newman <william.newman@seattlecolleges.edu>
2018-02-13 17:51:07 -0800

    Adds first version of Survey Class

--- surveys/survey_view.php
+++ surveys/survey_view.php
@@ -22,10 +22,16 @@ if(isset($_GET['id']) && (int)$_GET['id'] > 0){#proper data must be on querystri
 	myRedirect(VIRTUAL_PATH . "surveys/index.php");
 }
 
+$mySurvey = new Survey($myID);
+
+//dumpDie($mySurvey);
+
+
 //sql statement to select individual item
-$sql = "select Title,Description,DateAdded from wn18_surveys where SurveyID = " . $myID;
+//$sql = "select Title,Description,DateAdded from wn18_surveys where SurveyID = " . $myID;
 //---end config area --------------------------------------------------
 
+/*
 $foundRecord = FALSE; # Will change to true, if record found!
    
 # connection comes first in mysqli (improved) function
@@ -43,10 +49,11 @@ if(mysqli_num_rows($result) > 0)
 }
 
 @mysqli_free_result($result); # We're done with the data!
+*/
 
-if($foundRecord)
+if($mySurvey->IsValid)
 {#only load data if record found
-	$config->titleTag = $Title; #overwrite PageTitle with Muffin info!
+	$config->titleTag = $mySurvey->Title; #overwrite PageTitle with Muffin info!
 }
 /*
 $config->metaDescription = 'Web Database ITC281 class website.'; #Fills <meta> tags.
@@ -64,14 +71,13 @@ $config->nav1 = array("page.php"=>"New Page!") + $config->nav1; #add a new page 
 
 get_header(); #defaults to theme header or header_inc.php
 ?>
-<h3 align="center"><?=$Title;?></h3>
 <?php
-if($foundRecord)
+if($mySurvey->IsValid)
 {#records exist - show survey!
   echo '
-  <p>Title: title goes here</p>
-  <p>Description: description goes here</p>
-  <p>Date Added: date added goes here</p>
+  <h3 align="center">' . $mySurvey->Title . '</h3>
+  <p>Description: ' . $mySurvey->Description . '</p>
+  <p>Date Added: ' . $mySurvey->DateAdded . '</p>
   ';
 }else{//no such survey!
     echo '
@@ -80,4 +86,50 @@ if($foundRecord)
 }
 
 get_footer(); #defaults to theme footer or footer_inc.php
-?>
+
+class Survey
+{
+    public $SurveyID = 0;
+    public $Title = '';
+    public $Description = '';
+    public $DateAdded = '';
+    public $IsValid = false;
+    
+    
+    public function __construct($myID)
+    {
+        //cast the data to an integer
+        $this->SurveyID = (int)$myID;
+        
+        $sql = "select Title,Description,DateAdded from wn18_surveys where SurveyID = " . $this->SurveyID;
+   
+        # connection comes first in mysqli (improved) function
+        $result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
+
+        if(mysqli_num_rows($result) > 0)
+        {#records exist - process
+               $this->IsValid = true;	
+               while ($row = mysqli_fetch_assoc($result))
+               {
+                    $this->Title = dbOut($row['Title']);
+                    $this->Description = dbOut($row['Description']);
+                    $this->DateAdded = dbOut($row['DateAdded']);
+               }
+        }
+
+        @mysqli_free_result($result); # We're done with the data!
+        
+        
+        
+    }//end Survey constructor
+    
+}//end Survey class
+
+
+
+
+
+
+
+
+
